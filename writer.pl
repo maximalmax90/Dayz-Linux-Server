fd7ff6397ffe671fbf7d142d9f8b27b77e4c6770
Makeev Maxim <75565218+maximalmax90@users.noreply.github.com>
2022-04-10 06:12:04 +0800

    Update writer.pl

--- writer.pl
+++ writer.pl
@@ -501,6 +501,7 @@ sub h_player_update {
         }
     }
     if ($model) {
+        $model = '"'.$model.'"' if (length($model) > 3 && $model !~ m/"/);
         unless ( parse_json ($model) ) {
             print STDERR "Error h_player_update(): model invalid json!\n";
             $model = undef;
